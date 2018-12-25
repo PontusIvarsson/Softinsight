925455b23f8000e1487126a8948a10e7260d1563
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2018-12-25 16:41:11 +0100

    maria

--- src/WebApp/Data/ApplicationDbContext.cs
+++ src/WebApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApp.Domain;
 
 namespace WebApp.Data
 {
@@ -12,5 +13,7 @@ namespace WebApp.Data
             : base(options)
         {
         }
+
+        public DbSet<MarkdownPost> MarkdownPosts { get; set; }
     }
 }
---
+++ src/WebApp/Domain/MarkdownPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Domain
+{
+    public class MarkdownPost
+    {
+        public int ID { get; set; }
+        public string Markdown { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}

ab9c3ad27e516aad7f6ef6dff0146f6d723204e3
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2018-12-25 21:12:15 +0100

    migration

---
+++ src/WebApp/Data/Migrations/20181225201156_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Data.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestProperty",
+                table: "MarkdownPosts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestProperty",
+                table: "MarkdownPosts");
+        }
+    }
+}

ab752ca59c385e69bd7699416e6548b2f60731a4
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2018-12-26 22:39:16 +0100

    core 2.2 update

---
+++ src/WebApp/Domain/Blog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Domain
+{
+    public class Blog
+    {
+        public Blog(string name)
+        {
+            Name = name;
+        }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        private List<Insight> _insights;
+        public ICollection<Insight> Insights { get; set; }
+
+        public void AddInsight(string text)
+        {
+            Insight i = new Insight(text);
+            _insights.Add(i);
+        }
+
+    }
+
+    public class Insight
+    {
+        private Insight() { }
+        public Insight(string text)
+        {
+            Text = text;
+            CreatedDate = DateTime.Now;
+        }
+        public int Id { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public string Text { get; private set; }
+
+        private List<Hashtag> _tags;
+
+        [NotMapped]
+        public ICollection<Hashtag> Hashtags { get; private set; }
+
+
+        public void AddTag(Hashtag tag)
+        {
+            if (_tags.Contains(tag))
+                throw new ApplicationException("Already contains tag.");
+            Hashtags.Add(tag);
+        }
+
+    }
+
+    public class Hashtag : ValueObject
+    {
+        public Hashtag(string value)
+        {
+            //todo: add valid validation =)
+            if (value.Contains(' '))
+            {
+
+                throw new ApplicationException("Tags can only contain a-z an");
+            }
+            Value = value;
+        }
+        public string Value { get; private set; }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+}
---
+++ src/WebApp/Migrations/20181226211641_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Insight",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insight", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Insight_Blog_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blog",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Insight_BlogId",
+                table: "Insight",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Insight");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}

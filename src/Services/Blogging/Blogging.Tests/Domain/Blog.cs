bfed6d5ce6330801b5745477f297daa6c4efd59d
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2019-01-03 15:10:50 +0100

    tests

---
+++ src/Services/Blogging/Blogging.Tests/Domain/Blog.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Tests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Blogging.Tests.Domain
+{
+    [Trait(TestHelper.TestType, TestHelper.IntegrationTest)]
+    public class Blog_Should
+    {
+
+        [Trait(TestHelper.TestType, TestHelper.UnitTest)]
+        [Fact]
+        public void Be_Created()
+        {
+
+        }
+
+        [Trait(TestHelper.TestType, TestHelper.IntegrationTest)]
+        [Fact]
+        public void Be_Created_In_DataBase()
+        {
+
+        }
+    }
+}

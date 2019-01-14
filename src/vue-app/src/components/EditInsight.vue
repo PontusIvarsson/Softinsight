8392b4dd0f52da9e904905bff75d4018d48534fa
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2019-01-14 11:28:48 +0100

    eslint

--- src/vue-app/src/components/EditInsight.vue
+++ src/vue-app/src/components/EditInsight.vue
@@ -1,36 +1,59 @@
 <template>
-<div>
-  <h1>{{insightName}}</h1>
-  <textarea :value="content"></textarea>
-  <input type="button" v-on:click="al('hoho')" value="Save"/>
 
-  <hr>
-  View output:<br>
-  <div v-bind:text="content"></div>
-  <span :value="content"/>
-  <input :value="content" />
-</div>
+  <div>
+  <h1>{{insightName}}</h1>
+
+  <input type="checkbox" v-model="editMode">
+
+  <div v-if="editMode">
+    <div v-html="content" v-bind:class="'color-' + color"> </div>
+  </div>
+
+  <div v-if="!editMode">
+    <textarea v-model="content"></textarea>
+    <input type="button" v-on:click="al(content)" value="Save"/>
+  </div>
+
+
+  <ul>
+      <li v-for="hashtag in hashtags" v-bind:key="hashtag">
+        <span v-bind:text="content"></span>
+        {{hashtag}}
+      </li>
+  </ul>
+
+    View output:<br>
+    {{content}}
+  </div>
+
 </template>
 
 <script>
-export default{
+export default {
   name: 'EditInsight',
   props: {
     insightName: String,
   },
   data() {
-    return{
-    content: "wow det här är content asdf"
-    }
+    return {
+      color: 'red',
+      editMode: 'true',
+      content: 'wow det här är content',
+      hashtags: ['#tag1', '#tag2', '#tag3'],
+    };
   },
   methods: {
-    al(message){
-      console.log("test");
-      alert(message);
+    al(message) {
+      console.log(message);
+      this.content = `${message}a`;
+      this.hashtags.push('#newtag');
     },
   },
-}
+};
 </script>
 
 <style>
+  .color-red {
+    color: green;
+  }
 </style>
